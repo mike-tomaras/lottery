fa158ef9f9f21ae925c8a5c8790a6a7669038dcb
mike@incepted.io <mike@incepted.io>
2025-02-14 11:58:15 +0000

    Refactor to file-scoped namespace and update Presentation class

--- lottery.presentation.console/Presentation.cs
+++ lottery.presentation.console/Presentation.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace lottery.presentation.console;
 
-namespace lottery.presentation.console
+internal class Presentation
 {
-    internal class Presentation
-    {
-    }
 }
